cf0261cb123c9d1a2a5c971c4338b2c7cd4da99c
aidera <hope.aidera@gmail.com>
2022-07-29 15:08:12 +0300

    Removed sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 import 'antd/dist/antd.min.css';
 import { Layout, Menu } from 'antd';
 import { Content, Footer, Header } from 'antd/lib/layout/layout';
-import Sider from 'antd/lib/layout/Sider';
+import Title from 'antd/lib/typography/Title';
 import { ItemType } from 'antd/lib/menu/hooks/useItems';
 import Glossary from './pages/Glossary';
 
@@ -21,25 +21,20 @@ function App() {
 
   return (
     <Layout style={{ minHeight: '100vh' }}>
-      <Header className='site-layout-background' style={{ padding: 0 }} />
-      <Layout style={{ flexDirection: 'row' }}>
-        <Sider>
-          <div className='logo' />
-          <Menu
-            theme='dark'
-            defaultSelectedKeys={['1']}
-            mode='inline'
-            items={menuItems}
-          />
-        </Sider>
-        <Layout className='site-layout'>
-          <Content style={{ margin: '0 16px' }}>
-            <Glossary />
-          </Content>
-          <Footer style={{ textAlign: 'center' }}>
-            Ant Design ©2018 Created by Ant UED
-          </Footer>
-        </Layout>
+      <Header className='header' style={{ padding: 0 }}>
+        <div className='logo'>IT-interviewer</div>
+        <Menu
+          theme='dark'
+          mode='horizontal'
+          defaultSelectedKeys={['1']}
+          items={menuItems}
+        />
+      </Header>
+      <Layout className='site-layout'>
+        <Content style={{ margin: '0 16px' }}>
+          <Glossary />
+        </Content>
+        <Footer style={{ textAlign: 'center' }}></Footer>
       </Layout>
     </Layout>
   );
