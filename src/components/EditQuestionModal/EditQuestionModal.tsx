62b756e960065a63c7daacfcee597c2c8b675e35
aidera <hope.aidera@gmai.com>
2022-07-31 16:19:54 +0300

    Added basic form to add question modal

--- src/components/EditQuestionModal/EditQuestionModal.tsx
+++ src/components/EditQuestionModal/EditQuestionModal.tsx
@@ -4,8 +4,10 @@ import React, {
   useImperativeHandle,
   useState,
 } from 'react';
-import { Modal } from 'antd';
+import { Form, Input, InputNumber, Modal, Select } from 'antd';
 import { EditTypeEnum } from '../../models/utils';
+import { CATEGORIES } from '../../data/categories';
+import { QuizletQuestionCategory } from '../../models/category';
 
 type PropsType = {
   onOkCallback?: () => void;
@@ -25,6 +27,10 @@ const EditQuestionModal = forwardRef(
     const [isModalVisible, setIsModalVisible] = useState(false);
     const [title, setTitle] = useState('');
 
+    const categories: QuizletQuestionCategory[] = JSON.parse(
+      JSON.stringify(CATEGORIES),
+    );
+
     useImperativeHandle(ref, () => ({
       openModal(modalProps: OpenModalType) {
         setTitle(
@@ -39,10 +45,59 @@ const EditQuestionModal = forwardRef(
     };
 
     return (
-      <Modal title={title} visible={isModalVisible} onCancel={closeModal}>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
+      <Modal
+        title={title}
+        visible={isModalVisible}
+        onCancel={closeModal}
+        okText='Submit'
+      >
+        <Form
+          labelCol={{ span: 4 }}
+          wrapperCol={{ span: 20 }}
+          initialValues={{ remember: true }}
+          autoComplete='off'
+        >
+          <Form.Item
+            label='Title'
+            name='title'
+            rules={[{ required: true, message: 'Please input a title!' }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            name='category'
+            label='Category'
+            rules={[{ required: true }]}
+          >
+            <Select placeholder='Select a option and change input text above'>
+              {categories.map((category) => {
+                return (
+                  <Select.Option value={category.id} key={category.id}>
+                    {category.label}
+                  </Select.Option>
+                );
+              })}
+            </Select>
+          </Form.Item>
+
+          <Form.Item
+            label='Level'
+            name='level'
+            rules={[{ required: true, message: 'Please input a level!' }]}
+          >
+            <InputNumber
+              max={10}
+              min={1}
+              step={1}
+              formatter={(value) => `${value}`.replaceAll('.', '')}
+            />
+          </Form.Item>
+
+          <Form.Item label='Answer' name='answer'>
+            <Input.TextArea rows={4} />
+          </Form.Item>
+        </Form>
       </Modal>
     );
   },
