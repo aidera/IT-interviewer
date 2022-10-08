11659f564ddcded368fcbb1fcd8bc78875ca1407
aidera <hope.aidera@gmail.com>
2022-10-08 19:39:45 +0300

    Added cateories toolbar test and fixed some issues

---
+++ src/components/CategoriesToolbar/CategoriesToolbar.test.tsx
@@ -0,0 +1,210 @@
+import { act, render, screen, waitFor } from '@testing-library/react';
+import { observable } from 'mobx';
+import { Provider } from 'mobx-react';
+import userEvent from '@testing-library/user-event';
+import { useMediaQuery } from '@react-hook/media-query';
+
+import CategoriesToolbar from './CategoriesToolbar';
+import classes from './CategoriesToolbar.module.scss';
+
+jest.mock('@react-hook/media-query', () => ({
+  ...jest.requireActual('@react-hook/media-query'),
+  useMediaQuery: jest.fn().mockReturnValue(false),
+}));
+
+const categoriesStoreSetFilters = jest.fn();
+const categoriesStoreClearFilters = jest.fn();
+
+describe('CategoriesToolbar', () => {
+  let categoriesStore: any;
+
+  beforeAll(() => {
+    categoriesStore = observable({
+      filters: { title: '' },
+      setFilters: categoriesStoreSetFilters,
+      clearFilters: categoriesStoreClearFilters,
+    });
+  });
+
+  test('not display "Filters" button if it\'s not a mobile view', async () => {
+    const view = render(
+      <Provider categoriesStore={categoriesStore}>
+        <CategoriesToolbar />
+      </Provider>,
+    );
+
+    const filtersButton = view.queryByRole('button', {
+      name: /Filters/,
+    });
+
+    expect(filtersButton).not.toBeInTheDocument();
+  });
+
+  test('display "Filters" button if it\'s a mobile view', async () => {
+    (useMediaQuery as unknown as jest.Mock).mockReturnValue(true);
+
+    const view = render(
+      <Provider categoriesStore={categoriesStore}>
+        <CategoriesToolbar />
+      </Provider>,
+    );
+
+    const filtersButton = view.queryByRole('button', {
+      name: /Filters/,
+    });
+
+    expect(filtersButton).toBeInTheDocument();
+  });
+
+  test('there are no visible filters at the beginning if the view is mobile', async () => {
+    (useMediaQuery as unknown as jest.Mock).mockReturnValue(true);
+
+    const view = render(
+      <Provider categoriesStore={categoriesStore}>
+        <CategoriesToolbar />
+      </Provider>,
+    );
+
+    const filters = view.queryByTestId('filters');
+
+    expect(filters).toHaveClass(classes.filtersClosed);
+  });
+
+  test('display filters by clicking on filters button if the view is mobile', async () => {
+    (useMediaQuery as unknown as jest.Mock).mockReturnValue(true);
+
+    const view = render(
+      <Provider categoriesStore={categoriesStore}>
+        <CategoriesToolbar />
+      </Provider>,
+    );
+
+    const filtersButton = view.queryByRole('button', {
+      name: /Filters/,
+    });
+
+    userEvent.click(filtersButton as HTMLElement);
+
+    const filters = view.queryByTestId('filters');
+
+    expect(filters).not.toHaveClass(classes.filtersClosed);
+  });
+
+  test('display the title filter', async () => {
+    const view = render(
+      <Provider categoriesStore={categoriesStore}>
+        <CategoriesToolbar />
+      </Provider>,
+    );
+
+    const titleFilter = view.queryByPlaceholderText(/Search by title/i);
+
+    expect(titleFilter).toBeInTheDocument();
+  });
+
+  test('execute category set filters store function with expected parameters', async () => {
+    const view = render(
+      <Provider categoriesStore={categoriesStore}>
+        <CategoriesToolbar />
+      </Provider>,
+    );
+
+    const titleFilter = view.queryByPlaceholderText(/Search by title/i);
+
+    userEvent.type(titleFilter as HTMLElement, 'a');
+
+    expect(categoriesStoreSetFilters).toBeCalled();
+    expect(categoriesStoreSetFilters).toBeCalledTimes(1);
+    expect(categoriesStoreSetFilters).toBeCalledWith('title', 'a');
+  });
+
+  test('display the clear filters button', async () => {
+    const view = render(
+      <Provider categoriesStore={categoriesStore}>
+        <CategoriesToolbar />
+      </Provider>,
+    );
+
+    const clearButton = view.queryByLabelText(/clear filters/i);
+
+    expect(clearButton).toBeInTheDocument();
+  });
+
+  test('execute category clear filters store function', async () => {
+    const view = render(
+      <Provider categoriesStore={categoriesStore}>
+        <CategoriesToolbar />
+      </Provider>,
+    );
+
+    const clearButton = view.queryByLabelText(/clear filters/i);
+
+    userEvent.click(clearButton as HTMLElement);
+
+    expect(categoriesStoreClearFilters).toBeCalled();
+    expect(categoriesStoreClearFilters).toBeCalledTimes(1);
+  });
+
+  test('display add category button', async () => {
+    const view = render(
+      <Provider categoriesStore={categoriesStore}>
+        <CategoriesToolbar />
+      </Provider>,
+    );
+
+    const addButton = view.queryByRole('button', { name: /add category/i });
+
+    expect(addButton).toBeInTheDocument();
+  });
+
+  test('display dialog by clicking on add category button', async () => {
+    const view = render(
+      <Provider categoriesStore={categoriesStore}>
+        <CategoriesToolbar />
+      </Provider>,
+    );
+
+    const modalBefore = screen.queryByRole('dialog');
+    expect(modalBefore).not.toBeInTheDocument();
+
+    const addButton = view.queryByRole('button', { name: /add category/i });
+    userEvent.click(addButton as HTMLElement);
+
+    const modalAfter = screen.queryByRole('dialog');
+    expect(modalAfter).toBeInTheDocument();
+  });
+
+  test('display "More actions" button', async () => {
+    const view = render(
+      <Provider categoriesStore={categoriesStore}>
+        <CategoriesToolbar />
+      </Provider>,
+    );
+
+    const moreActionsButton = view.queryByLabelText(/more actions button/);
+
+    expect(moreActionsButton).toBeInTheDocument();
+  });
+
+  test('display menu by clicking on "More actions" button', async () => {
+    const view = render(
+      <Provider categoriesStore={categoriesStore}>
+        <CategoriesToolbar />
+      </Provider>,
+    );
+
+    const moreActionsMenuBefore = screen.queryByLabelText(/more actions menu/);
+    expect(moreActionsMenuBefore).not.toBeInTheDocument();
+
+    const moreActionsButton = view.queryByLabelText(/more actions button/);
+
+    act(() => {
+      userEvent.click(moreActionsButton as HTMLElement);
+    });
+
+    await waitFor(() => {
+      const moreActionsMenuAfter = screen.getByLabelText(/more actions menu/);
+      expect(moreActionsMenuAfter).toBeInTheDocument();
+    });
+  });
+});
--- src/components/CategoriesToolbar/CategoriesToolbar.tsx
+++ src/components/CategoriesToolbar/CategoriesToolbar.tsx
@@ -1,5 +1,5 @@
 import React, { ElementRef, useRef, useState } from 'react';
-import { observer } from 'mobx-react';
+import { inject, observer } from 'mobx-react';
 import { Button, Dropdown, Input, Menu, MenuProps } from 'antd';
 import {
   CloseOutlined,
@@ -13,188 +13,211 @@ import { saveAs } from 'file-saver';
 import { useMediaQuery } from '@react-hook/media-query';
 
 import classes from './CategoriesToolbar.module.scss';
-import { categoriesStore } from '../../store';
+import CategoriesStore from '../../store/categories.store';
 import { EditTypeEnum } from '../../models/utils.model';
 import EditCategoryModal from '../../components/EditCategoryModal/EditCategoryModal';
 import AddOrOverwriteConfirmModal from '../AddOrOverwriteConfirmModal/AddOrOverwriteConfirmModal';
 
-const CategoriesToolbar = () => {
-  const editModalRef = useRef<ElementRef<typeof EditCategoryModal>>(null);
-  const addOrOverwriteModalRef =
-    useRef<ElementRef<typeof AddOrOverwriteConfirmModal>>(null);
-  const uploadFileInput = useRef<HTMLInputElement>(null);
-
-  const [uploadFile, setUploadFile] = useState<string | null>(null);
-  const [isMobileFiltersOpen, setIsMobileFiltersOpen] =
-    useState<boolean>(false);
-
-  const isMobileView = useMediaQuery('(max-width: 700px)');
-
-  const onClearFiltersClick = () => {
-    categoriesStore.clearFilters();
-  };
-
-  const openAddCategoryModal = () => {
-    if (editModalRef.current) {
-      editModalRef.current.openModal({ type: EditTypeEnum.add });
-    }
-  };
-
-  const uploadFromJSON = () => {
-    uploadFileInput?.current?.click();
-  };
-
-  const uploadFromJSONHandleChange = (
-    e: React.ChangeEvent<HTMLInputElement>,
-  ) => {
-    const fileReader = new FileReader();
-    if (e?.target?.files?.[0]) {
-      fileReader.readAsText(e.target.files[0], 'UTF-8');
-    }
-    fileReader.onload = (e) => {
-      if (e?.target?.result) {
-        setUploadFile(e.target.result as string);
-        addOrOverwriteModalRef.current?.openModal();
-      } else {
-        setUploadFile(null);
-      }
-    };
-  };
-
-  const bulkUpload = (type: 'add' | 'overwrite') => {
-    if (!uploadFile) {
-      return;
-    }
-    const callback = () => {
-      if (uploadFileInput?.current) {
-        uploadFileInput.current.value = '';
-      }
-      setUploadFile(null);
-    };
-    categoriesStore.uploadBulkCategories(
-      type,
-      JSON.parse(uploadFile),
-      callback,
-    );
-  };
-
-  const downloadToJSON = () => {
-    const data = new Blob([JSON.stringify(categoriesStore.categories)], {
-      type: 'text/plain;charset=utf-8',
-    });
-    saveAs(data, 'categories.json');
-  };
-
-  const handleMoreActionsMenuClick: MenuProps['onClick'] = (e) => {
-    switch (e.key) {
-      case 'download':
-        downloadToJSON();
-        break;
-      case 'upload':
-        uploadFromJSON();
-        break;
-    }
-  };
-
-  const moreActionsMenu = (
-    <Menu
-      onClick={handleMoreActionsMenuClick}
-      items={[
-        {
-          label: 'Download (.json)',
-          key: 'download',
-          icon: <DownloadOutlined />,
-        },
-        {
-          label: 'Upload (.json)',
-          key: 'upload',
-          icon: <UploadOutlined />,
-        },
-      ]}
-    />
-  );
-
-  return (
-    <>
-      <div className={classes.toolbar}>
-        {isMobileView && (
-          <div className={classes.mobileFiltersTrigger}>
-            <Button
-              onClick={() => setIsMobileFiltersOpen(!isMobileFiltersOpen)}
-              type='link'
-              icon={isMobileFiltersOpen ? <UpOutlined /> : <DownOutlined />}
-            >
-              Filters
-            </Button>
-          </div>
-        )}
-
-        <div
-          className={
-            classes.filters +
-            ' ' +
-            (!isMobileFiltersOpen ? classes.filtersClosed : '')
-          }
-        >
-          {!isMobileView && <span>Filters: </span>}
-
-          <Input
-            placeholder='Search by title...'
-            allowClear
-            value={categoriesStore.filters.title}
-            onChange={(e) => {
-              categoriesStore.setFilters('title', e.target.value);
-            }}
-          />
-
-          {!isMobileView ? (
-            <Button
-              onClick={onClearFiltersClick}
-              title='Clear filters'
-              type='text'
-              icon={<CloseOutlined />}
-            ></Button>
-          ) : (
-            <Button onClick={onClearFiltersClick}>Clear filters</Button>
-          )}
-        </div>
-
-        <div className={classes.buttonsContainer}>
-          <Button type='primary' onClick={openAddCategoryModal}>
-            Add category
-          </Button>
-          <Dropdown
-            overlay={moreActionsMenu}
-            placement='bottomRight'
-            arrow={{ pointAtCenter: true }}
-          >
-            {!isMobileView ? (
-              <Button icon={<EllipsisOutlined />}></Button>
-            ) : (
-              <Button>More actions</Button>
-            )}
-          </Dropdown>
-        </div>
-      </div>
-
-      <div style={{ display: 'none' }}>
-        <input
-          type='file'
-          id='upload-from-json'
-          accept='.json'
-          ref={uploadFileInput}
-          onChange={uploadFromJSONHandleChange}
-        />
-      </div>
-
-      <EditCategoryModal ref={editModalRef} />
-      <AddOrOverwriteConfirmModal
-        ref={addOrOverwriteModalRef}
-        onOverwriteSelected={() => bulkUpload('overwrite')}
-        onAddSelected={() => bulkUpload('add')}
-      />
-    </>
-  );
+type PropsType = {
+  categoriesStore?: CategoriesStore;
 };
 
-export default observer(CategoriesToolbar);
+const CategoriesToolbar = inject('categoriesStore')(
+  observer((props: PropsType) => {
+    const editModalRef = useRef<ElementRef<typeof EditCategoryModal>>(null);
+    const addOrOverwriteModalRef =
+      useRef<ElementRef<typeof AddOrOverwriteConfirmModal>>(null);
+    const uploadFileInput = useRef<HTMLInputElement>(null);
+
+    const [uploadFile, setUploadFile] = useState<string | null>(null);
+    const [isMobileFiltersOpen, setIsMobileFiltersOpen] =
+      useState<boolean>(false);
+
+    const isMobileView = useMediaQuery('(max-width: 700px)');
+
+    const onClearFiltersClick = () => {
+      props.categoriesStore?.clearFilters();
+    };
+
+    const openAddCategoryModal = () => {
+      if (editModalRef.current) {
+        editModalRef.current.openModal({ type: EditTypeEnum.add });
+      }
+    };
+
+    const uploadFromJSON = () => {
+      uploadFileInput?.current?.click();
+    };
+
+    const uploadFromJSONHandleChange = (
+      e: React.ChangeEvent<HTMLInputElement>,
+    ) => {
+      const fileReader = new FileReader();
+      console.log(1);
+      if (e?.target?.files?.[0]) {
+        console.log(2);
+        fileReader.readAsText(e.target.files[0], 'UTF-8');
+      }
+      fileReader.onload = (e) => {
+        console.log(3);
+        if (e?.target?.result) {
+          console.log(4);
+          setUploadFile(e.target.result as string);
+          addOrOverwriteModalRef.current?.openModal();
+        } else {
+          setUploadFile(null);
+        }
+      };
+    };
+
+    const bulkUpload = (type: 'add' | 'overwrite') => {
+      if (!uploadFile) {
+        return;
+      }
+      const callback = () => {
+        if (uploadFileInput?.current) {
+          uploadFileInput.current.value = '';
+        }
+        setUploadFile(null);
+      };
+      props.categoriesStore?.uploadBulkCategories(
+        type,
+        JSON.parse(uploadFile),
+        callback,
+      );
+    };
+
+    const downloadToJSON = () => {
+      const data = new Blob(
+        [JSON.stringify(props.categoriesStore?.categories)],
+        {
+          type: 'text/plain;charset=utf-8',
+        },
+      );
+      saveAs(data, 'categories.json');
+    };
+
+    const handleMoreActionsMenuClick: MenuProps['onClick'] = (e) => {
+      switch (e.key) {
+        case 'download':
+          downloadToJSON();
+          break;
+        case 'upload':
+          uploadFromJSON();
+          break;
+      }
+    };
+
+    const moreActionsMenu = (
+      <Menu
+        aria-label='more actions menu'
+        onClick={handleMoreActionsMenuClick}
+        items={[
+          {
+            label: 'Download (.json)',
+            key: 'download',
+            icon: <DownloadOutlined />,
+          },
+          {
+            label: 'Upload (.json)',
+            key: 'upload',
+            icon: <UploadOutlined />,
+          },
+        ]}
+      />
+    );
+
+    return (
+      <>
+        <div className={classes.toolbar}>
+          {isMobileView && (
+            <div className={classes.mobileFiltersTrigger}>
+              <Button
+                onClick={() => setIsMobileFiltersOpen(!isMobileFiltersOpen)}
+                type='link'
+                icon={isMobileFiltersOpen ? <UpOutlined /> : <DownOutlined />}
+              >
+                Filters
+              </Button>
+            </div>
+          )}
+
+          <div
+            className={
+              classes.filters +
+              ' ' +
+              (!isMobileFiltersOpen ? classes.filtersClosed : '')
+            }
+            data-testid='filters'
+          >
+            {!isMobileView && <span>Filters: </span>}
+
+            <Input
+              placeholder='Search by title...'
+              allowClear
+              value={props.categoriesStore?.filters.title}
+              onChange={(e) => {
+                props.categoriesStore?.setFilters('title', e.target.value);
+              }}
+            />
+
+            {!isMobileView ? (
+              <Button
+                onClick={onClearFiltersClick}
+                title='Clear filters'
+                type='text'
+                icon={<CloseOutlined />}
+                aria-label='clear filters'
+              ></Button>
+            ) : (
+              <Button onClick={onClearFiltersClick} aria-label='clear filters'>
+                Clear filters
+              </Button>
+            )}
+          </div>
+
+          <div className={classes.buttonsContainer}>
+            <Button type='primary' onClick={openAddCategoryModal}>
+              Add category
+            </Button>
+            <Dropdown
+              overlay={moreActionsMenu}
+              placement='bottomRight'
+              arrow={{ pointAtCenter: true }}
+              trigger={['click']}
+            >
+              {!isMobileView ? (
+                <Button
+                  icon={<EllipsisOutlined />}
+                  aria-label='more actions button'
+                ></Button>
+              ) : (
+                <Button aria-label='more actions button'>More actions</Button>
+              )}
+            </Dropdown>
+          </div>
+        </div>
+
+        <div style={{ display: 'none' }}>
+          <input
+            type='file'
+            id='upload-from-json'
+            accept='.json'
+            ref={uploadFileInput}
+            onChange={uploadFromJSONHandleChange}
+            aria-label='upload file'
+          />
+        </div>
+
+        <EditCategoryModal ref={editModalRef} />
+        <AddOrOverwriteConfirmModal
+          ref={addOrOverwriteModalRef}
+          onOverwriteSelected={() => bulkUpload('overwrite')}
+          onAddSelected={() => bulkUpload('add')}
+        />
+      </>
+    );
+  }),
+);
+
+export default CategoriesToolbar;
